8e3603fffaa33e63cbf60d644079826de6db8b0f
calvin <calr0x@gmail.com>
2021-07-03 14:47:42 -0500

    .

--- disk_check/disk_check.sh
+++ disk_check/disk_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# This script checks disk space of /dev/sda and alerts when its greater than the threshold that's set (75% default)
+# This script checks disk space of /dev/sda and alerts when its greater than the threshold that's set (90% default)
 # Setting to change:
 # SPACE_THRESHOLD: Set this to what percentage it should alert above.
 
-SPACE_THRESHOLD="75"
+SPACE_THRESHOLD="90"
 
 SPACE=$(df -m / | grep sda | awk '{print $5}')
 SPACE=$(sed 's|%||' <<< $SPACE)
