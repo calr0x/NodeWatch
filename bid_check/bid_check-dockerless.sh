8ae6b81fa745896722dfcb11ec5332ac2328c89b
calvin <calr0x@gmail.com>
2021-07-10 13:08:23 -0500

    .

--- bid_check/bid_check-dockerless.sh
+++ bid_check/bid_check-dockerless.sh
@@ -27,8 +27,8 @@ if [ $BID_CHECK_ENABLED == "true" ]
 then
   for i in "${OFFER_ID[@]}"
   do
-    TOKEN_ARRAY=($(curl -sX GET "https://v5api.othub.info/api/Job/detail/0x11c27fbecc3e384cdec966a6be83c97b2a1109ab1d4611eb118b9a5094b97a80" -H  "accept: text/plain" | jq '.TokenAmountPerHolder' | cut -d'"' -f2))
-    JOBTIME_ARRAY=($(curl -sX GET "https://v5api.othub.info/api/Job/detail/0x11c27fbecc3e384cdec966a6be83c97b2a1109ab1d4611eb118b9a5094b97a80" -H  "accept: text/plain" | jq '.HoldingTimeInMinutes'))
+    TOKEN_ARRAY=($(curl -sX GET "https://v5api.othub.info/api/Job/detail/$OFFER_ID" -H  "accept: text/plain" | jq '.TokenAmountPerHolder' | cut -d'"' -f2))
+    JOBTIME_ARRAY=($(curl -sX GET "https://v5api.othub.info/api/Job/detail/$OFFER_ID" -H  "accept: text/plain" | jq '.HoldingTimeInMinutes'))
     DAYS=$(expr ${JOBTIME_ARRAY[@]} / 60 / 24)
     /root/OT-NodeWatch/data/send.sh "Job awarded: $DAYS days at ${TOKEN_ARRAY[@]} TRAC"
   done
