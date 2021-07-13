6c43c6f5f41b51955b6f3fbf0fa95ba814249c70
calvin <calr0x@gmail.com>
2021-07-13 07:51:16 -0500

    .

--- bid_check/bid_check-dockerless.sh
+++ bid_check/bid_check-dockerless.sh
@@ -14,12 +14,8 @@ BIDS=$(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep Accepting |
 #echo Bids: $BIDS
 
 if [ $BIDS -eq 0 ]; then
-<<<<<<< HEAD
-  /root/OT-Settings/data/send.sh "Has not bid since $CHECK_INTERVAL"
-=======
   /root/OT-NodeWatch/data/send.sh "Has not bid since $CHECK_INTERVAL, restarting node"
   systemctl restart otnode
->>>>>>> 0b3a512da191c4a19fe5c4a4d5620eff4eab513e
 fi
 
 JOBS=$(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep 've been chosen' | wc -l)
