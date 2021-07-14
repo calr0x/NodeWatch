4a4b4443e3ba3431ea0ccb230a38494b69244181
calvin <calr0x@gmail.com>
2021-07-14 08:06:56 -0500

    Add suppoert for OT-Settings

--- bid_check/bid_check.sh
+++ bid_check/bid_check.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 # This script checks the logs for "Accepting" which indicates a bid.
-# Setting to change:
-# BID_CHECK_ENABLED: Set to false to disable bid notifications.
-# CHECK_INTERVAL: Set this to how far back to search the log for mentions of "Accepting".
+# Setting to change in OT-Settings/config.sh:
+# BID_CHECK_JOB_NOTIFY_ENABLED: Set to false to disable bid notifications (default true)
+# BID_CHECK_INTERVAL: Set this to how far back to search the log for mentions of "Accepting" (default 1 hour).
 # This value should match the CRON schedule. For example, Every 1 hour
 # CRON should run this script which checks the logs for the past 1 hour.
 
-CHECK_INTERVAL="1 hour ago"
-BID_CHECK_ENABLED="true"
+source /root/OT-Settings/config.sh
 
-BIDS=$(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep Accepting | wc -l)
+BIDS=$(journalctl -u otnode.service --since "$BID_CHECK_INTERVAL" | grep Accepting | wc -l)
 #echo Bids: $BIDS
 
 if [ $BIDS -eq 0 ]; then
-  /root/OT-NodeWatch/data/send.sh "Has not bid since $CHECK_INTERVAL, restarting node"
+  /root/OT-NodeWatch/data/send.sh "Has not bid since BID_CHECK_INTERVAL, restarting node"
   systemctl restart otnode
 fi
 
-JOBS=$(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep 've been chosen' | wc -l)
+JOBS=$(journalctl -u otnode.service --since "BID_CHECK_INTERVAL" | grep 've been chosen' | wc -l)
 #echo Jobs: $JOBS
 
-OFFER_ID=($(journalctl -u otnode.service --since "$CHECK_INTERVAL" | grep 've been chosen' | grep -Eo '0x[a-z0-9]+'))
+OFFER_ID=($(journalctl -u otnode.service --since "BID_CHECK_INTERVAL" | grep 've been chosen' | grep -Eo '0x[a-z0-9]+'))
 
 #echo Array: ${#OFFER_ID[@]}
-if [ $BID_CHECK_ENABLED == "true" ]
+if [ $BID_CHECK_JOB_NOTIFY_ENABLED == "true" ]
 then
   for i in "${OFFER_ID[@]}"
   do
