a0f1a767e63b930b07d66e733e36250f0d071131
Valcyclovir <85747941+Valcyclovir@users.noreply.github.com>
2021-07-17 19:39:54 -0400

    +x

--- bid_check.sh
+++ bid_check.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # This script checks the logs for "Accepting" which indicates a bid.
-#
 # Setting to change in OT-Settings/config.sh:
 # BID_CHECK_JOB_NOTIFY_ENABLED: Set to false to disable bid notifications (default true)
 # BID_CHECK_INTERVAL: Set this to how far back to search the log for mentions of "Accepting" (default 1 hour).
